c118a02a42147a8ed78d54cb1c683bf1247cff8b
Sameh <>
2021-02-15 03:37:57 +0200

    add tab bar

---
+++ LadyBug/Core/Extensions/UIColor/UIColors.swift
@@ -0,0 +1,18 @@
+//
+//  UIColors.swift
+//  LadyBug
+//
+//  Created by Sameh on 2/15/21.
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var midGreen: UIColor {
+        return UIColor(red: 75.0/255.0, green: 174.0/255.0, blue: 79.0/255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var silver: UIColor {
+        return UIColor(red: 196.0/255.0, green: 209.0/255.0, blue: 212.0/255.0, alpha: 1.0)
+    }
+}
