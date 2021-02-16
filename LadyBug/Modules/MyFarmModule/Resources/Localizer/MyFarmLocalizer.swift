2c088916446dcf5cae4a8cd05ac8fa5b21be08e4
Mohamed Abdelhamed Ahmed <mohamedabdelhamed382@gmail.com>
2021-02-16 07:46:40 -0800

    make localize for my farm module , and add in Localizable arabic file

---
+++ LadyBug/Modules/MyFarmModule/Resources/Localizer/MyFarmLocalizer.swift
@@ -0,0 +1,95 @@
+//
+//  MyFarmLocalizer.swift
+//  LadyBug
+//
+//  Created by Mohamed Abdelhamed Ahmed on 2/15/21.
+//
+
+import Foundation
+
+struct MyFarmLocalizer {
+    var myFarmLocalizerSearchPlaceholderIc: String {
+        "my_farm_localizer_search_placeholder_ic".localized
+    }
+    var searchPlaceHolder: String {
+        "search_placeholder_ic".localized
+    }
+    var myFarmLocalizerTemperatureTitle: String {
+        "my_farm_localizer_temperature_title".localized
+    }
+    var myFarmLocalizerDateTitle: String {
+        "my_farm_localizer_date_title".localized
+    }
+    var myFarmLocalizerSunStateTitle: String {
+        "my_farm_localizer_sun_state_title".localized
+    }
+    var myFarmLocalizerTimeTitle: String {
+        "my_farm_localizer_time_title".localized
+    }
+    var myFarmLocalizerNotesTitle: String {
+        "my_farm_localizer_notes_title".localized
+    }
+    var myFarmLocalizerAboutNotesTitle: String {
+        "my_farm_localizer_about_notes_title".localized
+    }
+    var myFarmLocalizerPostTitle: String {
+        "my_farm_localizer_post_title".localized
+    }
+    var myFarmLocalizerTypeOfFarmTitle: String {
+        "my_farm_localizer_type_of_farm_title".localized
+    }
+    var myFarmLocalizerTheNumberOfAcresTitle: String {
+        "my_farm_localizer_the_number_of_acres_title".localized
+    }
+    var myFarmLocalizerChemicalSourceTitle: String {
+        "my_farm_localizer_chemical_source_title".localized
+    }
+    var myFarmLocalizerwidgetCodeTitle: String {
+        "my_farm_localizer_widget_code_title".localized
+    }
+    var myFarmLocalizerTheFarmLocationTitle: String {
+        "my_farm_localizer_the_farm_location_title".localized
+    }
+    var myFarmLocalizerTheNumberOfAcresValueTitle: String {
+        "my_farm_localizer_the_number_of_acres_value_title".localized
+    }
+    var myFarmLocalizerChemicalSourceValueTitle: String {
+        "my_farm_localizer_chemical_source_value_title".localized
+    }
+    var myFarmLocalizerHistoryOfCultivationValueTitle: String {
+        "my_farm_localizer_history_of_cultivation_value_title".localized
+    }
+    var myFarmLocalizerSeedlingSourceValueTitle: String {
+        "my_farm_localizer_seedling_source_value_title".localized
+    }
+    var myFarmLocalizerWidgetCodeValueTitle: String {
+        "my_farm_localizer_widget_code_value_title".localized
+    }
+    var myFarmLocalizerTheFarmLocationValueTitle: String {
+        "my_farm_localizer_the_farm_location_value_title".localized
+    }
+    var myFarmLocalizerTheNextMissionTitle: String {
+        "my_farm_localizer_the_next_mission_title".localized
+    }
+    var myFarmLocalizerAfterTwoDaysTitle: String {
+        "my_farm_localizer_after_two_days_title".localized
+    }
+    var myFarmLocalizerPlantingSeedsOnTerracesTitle: String {
+        "my_farm_localizer_planting_seeds_on_terraces_title".localized
+    }
+    var myFarmLocalizerWeightTitle: String {
+        "my_farm_localizer_weight_title".localized
+    }
+    var myFarmLocalizerLemonFarmTitle: String {
+        "my_farm_localizer_lemon_farm_title".localized
+    }
+    var myFarmLocalizerHypotheticalTitle: String {
+        "my_farm_localizer_hypothetical_title".localized
+    }
+    var myFarmLocalizerTodayTitle: String {
+        "my_farm_localizer_today_title".localized
+    }
+    var myFarmLocalizerAddYourFarmTitle: String {
+        "my_farm_localizer_add_your_farm_title".localized
+    }
+}
