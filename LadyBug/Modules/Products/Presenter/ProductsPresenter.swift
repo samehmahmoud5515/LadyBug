6b7ae5695634141a76579a08d9fec4df562e58fc
Sameh <>
2021-02-20 01:24:31 +0200

    products ui

---
+++ LadyBug/Modules/Products/Presenter/ProductsPresenter.swift
@@ -0,0 +1,26 @@
+//
+//  ProductsPresenter.swift
+//  LadyBug
+//
+//  Created by Sameh on 2/20/21.
+//
+
+import Foundation
+
+class ProductsPresenter: ProductsPresenterProtocol {
+    var localizer = ProductsLocalizer()
+    var images = ProductsImages()
+    
+    weak var view: ProductsViewProtocol?
+    
+    init(view: ProductsViewProtocol) {
+        self.view = view
+    }
+    
+    func attach() {
+    }
+    
+    func getProductsCount() -> Int {
+        return 5
+    }
+}
---
+++ LadyBug/Modules/Products/Resources/Images/ProductsImages.swift
@@ -0,0 +1,12 @@
+//
+//  ProductsImages.swift
+//  LadyBug
+//
+//  Created by Sameh on 2/20/21.
+//
+
+import Foundation
+
+class ProductsImages: CommonImages {
+    
+}
