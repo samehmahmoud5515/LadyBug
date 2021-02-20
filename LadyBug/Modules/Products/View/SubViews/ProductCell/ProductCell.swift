3ee6133acc2ed48a3bc0364c286e367c08a4a2f6
Sameh <>
2021-02-20 02:27:27 +0200

    products ui continued

--- LadyBug/Modules/Products/View/SubViews/ProductCell/ProductCell.swift
+++ LadyBug/Modules/Products/View/SubViews/ProductCell/ProductCell.swift
@@ -9,6 +9,23 @@ import UIKit
 
 class ProductCell: UITableViewCell {
 
+    @IBOutlet weak var productTitleLabel: UILabel!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
     
+    func setupUI() {
+        productTitleLabel.textColor = .purplishBrown
+        productTitleLabel.font = UIFont.get(enFont: .regular(12), arFont: .regular(12))
+        
+        productDescriptionLabel.textColor = .purplishBrown
+        productDescriptionLabel.font = UIFont.get(enFont: .light(10), arFont: .light(10))
+        
+        priceLabel.textColor = .midGreenTwo
+        priceLabel.font = UIFont.get(enFont: .regular(12), arFont: .regular(12))
+        
+        cityLabel.textColor = .midGreenTwo
+        cityLabel.font = UIFont.get(enFont: .regular(12), arFont: .regular(12))
+    }
     
 }
