9d6fa3ed232264b68b00ab856f3e99ad6b1991c0
SAMEH <sameh.saad2@vodafone.com>
2021-03-14 00:07:10 +0200

    replace add post cell

--- LadyBug/Modules/AddPost Module/View/SubViews/AddPostCell/AddPostCell.swift
+++ LadyBug/Modules/AddPost Module/View/SubViews/AddPostCell/AddPostCell.swift
@@ -9,17 +9,9 @@ import UIKit
 
 class AddPostCell: UITableViewCell {
 
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var selectionLabel: UILabel!
+    @IBOutlet weak var selectionView: StandardSelectionView!
     
     func setupUI(model: AddPostModel) {
-        titleLabel.text = model.title
-        selectionLabel.text = model.selectionTitle
-        
-        titleLabel.font = UIFont.get(enFont: .regular(16), arFont: .regular(16))
-        titleLabel.textColor = .purplishBrown
-        
-        selectionLabel.font = UIFont.get(enFont: .regular(15), arFont: .regular(15))
-        selectionLabel.textColor = .blueyGreyTwo
+        selectionView.setupUI(title: model.title, selectionTitle: model.selectionTitle)
     }
 }
